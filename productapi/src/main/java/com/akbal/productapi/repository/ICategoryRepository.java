06d0e8ff189a8ff9bd6ea19741bf3fc870811b8a
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2023-02-20 23:01:47 +0300

    api create

--- productapi/src/main/java/com/akbal/productapi/repository/ICategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.akbal.productapi.repository;
-
-import com.akbal.productapi.entity.Category;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICategoryRepository extends CrudRepository<Category,Integer> {
-}
